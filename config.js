2605d70204ac13f84f2927b5fd146e43aae73661
sixteeninter-ux <sixteeninter@gmail.com>
2026-02-12 18:58:28 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+window.APP_CONFIG = {
+  CHAIN_ID_HEX: "0x38",
+  CHAIN_ID_DEC: 56,
+  CHAIN_NAME: "BSC Mainnet",
+  RPC_URL: "https://bsc-dataseed.binance.org/",
+  EXPLORER: "https://bscscan.com",
+
+  // Contract
+  CONTRACT: "0x726AeA9104D606B6664d8ee5d5b53da36FBaFE86",
+
+  // Token addresses (ตามที่คุณให้มาก่อนหน้า)
+  STCEX: "0xAD01f5c7a87460229c3Dc70a7259E16FA1E260a2",
+  STC:   "0x12730990De18c6864e45d22f007EBebaFbf6c76b",
+};
